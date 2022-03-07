4c775c46c1e48a5aff029aa87f57e6c0a6d057fc
congshuang <cong_shuang666>
2022-03-07 22:43:11 +0800

    feat: 验证

--- packages/@vue/cli/__tests__/cli.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const cli = require('..');
-
-describe('@vue3/cli', () => {
-    it('needs tests');
-});
---
+++ packages/cli/lib/cli.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = cli
+
+function cli () {
+  const a = 1
+  console.log('----', a)
+  // TODO
+}
